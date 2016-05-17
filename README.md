789439bc0cfbb7bdf4097007e8712e3a776cc98b
Amine Benseddik <amine.benseddik@pixelfactory.com>
2016-05-18 00:58:18 +0200

    prepare for rancher

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rancher-percona-cluster
+[![Travis CI](https://img.shields.io/travis/amine7536/percona/master.svg)](https://travis-ci.org/amine7536/percona/branches)
+
+# Percona Ubuntu 14.04 Docker Image
+
+This is a clone of the Docker [official image](https://docs.docker.com/docker-hub/official_repos/) for [percona](https://registry.hub.docker.com/_/percona/). See [the Docker Hub page](https://registry.hub.docker.com/pixelfactory/percona/) for the full readme on how to use this Docker image and for information regarding contributing and issues.
+
+Docker official image is using Debian:jessie as the base image, this image is using Ubuntu Trusty instead.
---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+packagesUrl='http://repo.percona.com/apt/dists/trusty/main/binary-amd64/Packages'
+packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
+curl -sSL "${packagesUrl}.gz" | gunzip > "$packages"
+
+for version in "${versions[@]}"; do
+	cp start_pxc.sh "$version/start_pxc.sh"
+
+	fullVersion="$(grep -m1 -A10 "^Package: percona-server-server-$version\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	(
+		set -x
+		sed '
+			s/%%PERCONA_MAJOR%%/'"$version"'/g;
+			s/%%PERCONA_VERSION%%/'"$fullVersion"'/g;
+		' Dockerfile.template > "$version/Dockerfile"
+	)
+done
+
+rm "$packages"
